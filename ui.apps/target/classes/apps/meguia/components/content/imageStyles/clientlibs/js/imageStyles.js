e11daa99e5627f08ab601a97e1a096d7e309a52c
meguiaCone <mariana.eguia@conexiogroup.com>
2018-02-20 18:17:57 -0300

    dialog fix

---
+++ ui.apps/target/classes/apps/meguia/components/content/imageStyles/clientlibs/js/imageStyles.js
@@ -0,0 +1,11 @@
+$(document).ready(function () {
+    console.log("imageStyles ready");
+    $.fn.addClassImg = function(opts) {
+        if (opts.clase === "sepia") {
+            $("#imgS").addClass('sepia');
+        }
+        else if (opts.clase  === "byw"){
+            $("#imgS").addClass('byw');
+        }
+    }
+});
